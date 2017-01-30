5bc59362d0689feec7b76c089488f1bc38534c9a
Anthony <Knoll>
2017-01-30 16:50:23 -0700

    Integrated new and create actions for notebooks

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,6 +1,22 @@
 class NotebooksController < ApplicationController
   def index
     @notebooks_items = Notebook.all
+  end
 
- end
+  def new
+    @notebooks_items = Notebook.new
+  end
+
+  def create
+    @notebook_item = Notebook.new(params.require(:notebook).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @notebook_item.save
+        format.html { redirect_to notebooks_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+
+      end
+    end
+  end
 end
