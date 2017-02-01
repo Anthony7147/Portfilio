e18169ac65cbe0a49a98c05b589e1bc4e7afef0d
Anthony <Knoll>
2017-01-31 17:05:08 -0700

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :notebooks
+  resources :notebooks, except: [:show]
+  get 'notebook/:id', to: 'notebooks#show', as: 'notebook_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
